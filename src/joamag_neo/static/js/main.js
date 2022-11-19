7730a43ed821b84f97065ad3a2977991a5e2f0dc
João Magalhães <joamag@gmail.com>
2022-11-19 20:06:37 +0000

    feat: small color changes

--- src/joamag_neo/static/js/main.js
+++ src/joamag_neo/static/js/main.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
-    console.log("%cJoão Magalhães\n%cSoftware Engineer",
+    console.log("%cJoão Magalhães\n%cSoftware Engineer 👨‍💻",
         "font-weight: bold; font-size: 32px; color: #4d4d4d;",
-        "font-size: 16px; color: #4d4d4d;");
+        "font-size: 16px; color: #8d8d8d;");
     console.log(
         "If you reach this area you're probably interested in getting to know me, let's have a chat at joao@joao.me."
     );
