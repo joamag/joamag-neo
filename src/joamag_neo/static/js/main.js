fc11faa92a238f0b7afb0f9759d4ad0be2b454d5
João Magalhães <joamag@gmail.com>
2022-11-21 10:53:20 +0000

    feat: better logging of message

--- src/joamag_neo/static/js/main.js
+++ src/joamag_neo/static/js/main.js
@@ -1,8 +1,19 @@
 jQuery(document).ready(function() {
-    console.log("%cJoão Magalhães\n%cSoftware Engineer 👨‍💻",
-        "font-weight: bold; font-size: 32px; color: #4d4d4d;",
-        "font-size: 16px; color: #8d8d8d;");
+    const cssTitle = [
+        "font-weight: bold",
+        "font-size: 32px",
+        "color: #4d4d4d"
+    ];
+    const cssSubtitle = [
+        "font-size: 16px",
+        "color: #8d8d8d"
+    ];
+    const cssLink = [
+        "color: #4078c0"
+    ];
+    console.log("%cJoão Magalhães\n%cSoftware Engineer 👨‍💻", cssTitle.join(";"), cssSubtitle.join(";"));
     console.log(
-        "If you reach this area you're probably interested in getting to know me, let's have a chat at joao@joao.me."
+        "If you reach this area you're probably interested in getting to know me, let's have a chat at %cjoao@joao.me.",
+        cssLink.join(";")
     );
 });
